070601bb291b20b3f8407c2b5861a1e3f0b597dd
Cristina Matonte <anitsirc1@gmail.com>
2013-12-18 22:53:27 +0100

    Add missing requests_helper file…

---
+++ helpers/requests_helper.rb
@@ -0,0 +1,31 @@
+module RequestsHelper
+
+  #Avoids resending push notification to an already notified user.
+  def self.set_sent_helper helpers, request
+    helpers.each do |helper|
+      HelperRequest.create! :request => request, :helper => helper
+    end
+  end
+
+  def self.send_notifications request, device_tokens
+    puts request.inspect
+    # Create notification
+    user = request.blind
+    notification_args_name = user.to_s
+    notification = {
+        :device_tokens => device_tokens,
+        :aps => {
+            :alert => {
+                :"loc-key" => "PUSH_NOTIFICATION_ANSWER_REQUEST_MESSAGE",
+                :"loc-args" => [ notification_args_name ],
+                :"action-loc-key" => "PUSH_NOTIFICATION_ANSWER_REQUEST_ACTION",
+                :short_id => request.short_id,
+            },
+            :sound => "default"
+        }
+    }
+
+    # Send notification
+    Urbanairship.push(notification)
+  end
+end

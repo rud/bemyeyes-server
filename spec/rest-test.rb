8a36486ea79c5c05bb5d2d07b691828ef7f853ba
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-04-09 14:15:28 +0200

    indention

--- spec/rest-test.rb
+++ spec/rest-test.rb
@@ -8,23 +8,23 @@ describe "Rest api" do
     @username = config['authentication']['username']
     @password = config['authentication']['password']
   end
-describe "User api" do 
-  before(:each) do
-    config = YAML.load_file('config/config.yml')
-    @username = config['authentication']['username']
-    @password = config['authentication']['password']
+  describe "User api" do 
+    before(:each) do
+      config = YAML.load_file('config/config.yml')
+      @username = config['authentication']['username']
+      @password = config['authentication']['password']
+    end
+    
+    it "Should return a user" do
+      response = RestClient::Request.new(
+        :method => :get,
+        :url => "http://stagingapi.bemyeyes.org/users/1",
+        :user => @username,
+        :password => @password,
+        :headers => { :accept => :json,
+          :content_type => :json }
+          ).execute 
+      response.code.should eq(200)
+    end
   end
-  
-it "Should return a user" do
-response = RestClient::Request.new(
-    :method => :get,
-    :url => "http://stagingapi.bemyeyes.org/users/1",
-    :user => @username,
-    :password => @password,
-    :headers => { :accept => :json,
-    :content_type => :json }
-  ).execute 
-response.code.should eq(200)
-end
-end
 end

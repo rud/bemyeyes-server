12700c44f75c3acb5fed6df4b06f3847d1de7524
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-05-22 11:38:02 +0200

    make spec work again and again

--- spec/rest-test.rb
+++ spec/rest-test.rb
@@ -22,10 +22,11 @@ describe "Rest api" do
  describe "create user" do
      it "can create a user" do
 
+         email =  "user_#{(Time.now.to_f*100000).to_s}@example.com" 
          password = AESCrypt.encrypt('Password1', @security_salt)
          url = "http://#{@username}:#{@password}@localhost:9292/users/"
          response = RestClient.post url, {'first_name' =>'first_name', 
-             'last_name'=>'last_name', 'email'=> 'someone@example.com', 
+             'last_name'=>'last_name', 'email'=> email, 
              'role'=> 'helper', 'password'=> password }.to_json
          response.code.should eq(200)
      end

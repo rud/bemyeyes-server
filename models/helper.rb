3f3cc5b0482a2395538ea471e1a1f2785ed07831
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-06-04 14:13:38 +0200

    typo in helpers

--- models/helper.rb
+++ models/helper.rb
@@ -13,9 +13,9 @@ class Helper < User
   def available request=nil, limit=5
     request_id = request.present? ? request.id : nil
     contacted_helpers = HelperRequest.where(:request_id => request_id).fields(:helper_id).all.collect(&:helper_id)
-    logged_users = Token.where(:expiry_time.gt => Time.now).fields(:user_id).all.collect(&:user_id)
+    logged_out_users = Token.where(:expiry_time.gt => Time.now).fields(:user_id).all.collect(&:user_id)
     Helper.where(:id.nin => contacted_helpers,
-                 :id.in => logged_users,
+                 :id.in => logged_out_users,
                  "$or" => [
                      {:available_from => nil},
                      {:available_from.lt => Time.now.utc}

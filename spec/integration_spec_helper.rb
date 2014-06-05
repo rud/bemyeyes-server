d8b97828f8fd4824f5879bbc5ea8c735e36d4f23
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-06-05 13:49:36 +0200

    integraton spec don't need auth against mongo

--- spec/integration_spec_helper.rb
+++ spec/integration_spec_helper.rb
@@ -1,14 +1,10 @@
 require 'yaml'
+require 'mongo_mapper'
 
 class IntegrationSpecHelper
-	def self.InitializeMongo
-	  config = YAML.load_file('config/config.yml')
-	  MongoMapper.connection = Mongo::Connection.new(config['database']['host'])
-	  MongoMapper.database = config['database']['name']
-	  if config['database'].has_key? 'username'
- +         MongoMapper.connection[config['database']['name']].authenticate(config['database']['username'], config['database']['password'])
- +        else
- +          MongoMapper.connection[config['database']['name']]
- +        end
-	end
+  def self.InitializeMongo
+    config = YAML.load_file('config/config.yml')
+    MongoMapper.connection = Mongo::Connection.new(config['database']['host'])
+    MongoMapper.database = config['database']['name']
+  end
 end

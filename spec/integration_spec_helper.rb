1ad0398ec1e50d4c3c5940c1acd600a65bb40604
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-06-05 13:22:02 +0200

    Making semaphore work

--- spec/integration_spec_helper.rb
+++ spec/integration_spec_helper.rb
@@ -5,6 +5,11 @@ class IntegrationSpecHelper
 		config = YAML.load_file('config/config.yml')
 	    MongoMapper.connection = Mongo::Connection.new(config['database']['host'])
 	    MongoMapper.database = config['database']['name']
-	    MongoMapper.connection['bemyeyes'].authenticate(config['database']['username'], config['database']['password'])
+	     if db_config.has_key? 'username'
+ +            MongoMapper.connection[db_config['name']].authenticate(db_config['username'], db_config['password'])
+ +           else
+ +             MongoMapper.connection[db_config['name']]
+ +           end
+	
 	end
 end

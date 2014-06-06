b7e4abe5b64e4bada55fa7f4beac29400aa8e4a5
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-06-06 11:18:03 +0200

    working on logs

--- app.rb
+++ app.rb
@@ -29,10 +29,8 @@ class App < Sinatra::Base
   # Do any configurations
   configure do
     set :environment, :development
-    set :dump_errors, false
+    enable :logging, :dump_errors
     set :raise_errors, true
-    set :show_exceptions, true
-    enable :logging
     set :app_file, __FILE__
     set :config, YAML.load_file('config/config.yml') rescue nil || {}
     set :scheduler, Rufus::Scheduler.new
--- config.ru
+++ config.ru
@@ -2,4 +2,8 @@ require 'rubygems'
 require 'sinatra'
 require File.expand_path '../app.rb', __FILE__
 
+ log = File.new("./log/lowlevel.log", "a+")
+  $stdout.reopen(log)
+  $stderr.reopen(log)
+
 run App.new

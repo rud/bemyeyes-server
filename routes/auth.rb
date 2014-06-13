3e370d3c5d4ba909fd1d2bddadc9692a3d2ea346
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-06-13 15:56:58 +0200

    reject password reset for external (ie facebook) users

--- routes/auth.rb
+++ routes/auth.rb
@@ -25,6 +25,10 @@ class App < Sinatra::Base
         if user.nil?
           give_error(400, ERROR_INVALID_BODY, "User Not found")
         end
+
+        unless user.user_id.nil?
+          give_error(400, ERROR_INVALID_BODY, "cannot reset password for external user")
+        end
         token = create_reset_password_token user 
         mail_service = create_mail_service settings
         

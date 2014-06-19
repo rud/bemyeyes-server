9f9ff06e11dee6f829a83cf7812e8ae17862b74f
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-06-19 10:27:56 +0200

    make specs run again

--- spec/waiting_requests_spec.rb
+++ spec/waiting_requests_spec.rb
@@ -2,21 +2,25 @@ require 'factory_girl'
 
 require_relative './factories'
 require_relative '../helpers/waiting_requests'
+require_relative '../models/init'
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
 
 describe WaitingRequests do
+	before do
+	IntegrationSpecHelper.InitializeMongo()
+  end
 	before(:each) do
 		@sut = WaitingRequests.new
 		Request.destroy_all
 		request = build(:request)
-    	request.save
+    request.save
 	end
 
 	it "sends requests" do
 		requests = @sut.get_waiting_requests_from_lasts_2_minutes
-		requests.count.should eq(1)
+		expect(requests.count).to eq(1)
 	end
 end

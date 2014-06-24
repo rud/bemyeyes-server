b5774b0f2a5fa0cafae6daaa36b5ad555c2796b6
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-06-24 11:46:18 +0200

    added comment

--- models/user.rb
+++ models/user.rb
@@ -119,6 +119,7 @@ class User
   end
 
   def self.time_to_seconds_since_midnight time, utc_offset
+    #the utc offset should actually be subtracted - look at a map if in doubt
     hour_in_utc = time.hour - utc_offset
     hour_in_utc * 3600 + time.min * 60
   end

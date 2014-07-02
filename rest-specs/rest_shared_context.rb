42d059ac06d86c168e3344aaa799f1b5a24759cc
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-07-02 09:11:39 +0200

    fixed rest-specs

--- rest-specs/rest_shared_context.rb
+++ rest-specs/rest_shared_context.rb
@@ -29,11 +29,11 @@ shared_context "rest-context" do
     @password = AESCrypt.encrypt('Password1', @security_salt)
   end
 
-  def create_user
+  def create_user role ="helper"
     createUser_url = "#{@servername_with_credentials}/users/"
     response = RestClient.post createUser_url, {'first_name' =>'first_name',
                                                 'last_name'=>'last_name', 'email'=> @email,
-                                                'role'=> 'helper', 'password'=> @password }.to_json
+                                                'role'=> role, 'password'=> @password }.to_json
 
     jsn = JSON.parse response.body
     id = jsn['id']

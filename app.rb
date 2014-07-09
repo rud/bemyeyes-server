fd1ecc0a506e8c5619b353eb6b8e752dc8a4f4f1
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-07-09 16:03:32 +0200

    no need for the db to log

--- app.rb
+++ app.rb
@@ -54,7 +54,7 @@ class App < Sinatra::Base
     OpenTokSDK = OpenTok::OpenTok.new opentok_config['api_key'], opentok_config['api_secret']
 
     db_config = settings.config['database']
-    MongoMapper.connection = Mongo::Connection.new(db_config['host'], :logger => TheLogger.log)
+    MongoMapper.connection = Mongo::Connection.new(db_config['host'])
     MongoMapper.database = db_config['name']
     if db_config.has_key? 'username'
       MongoMapper.connection[db_config['name']].authenticate(db_config['username'], db_config['password'])

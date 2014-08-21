4da363414c52bb0a0ae8641d011ef62ce7d43c95
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-08-21 11:17:06 +0200

    clean up app.rb

--- app.rb
+++ app.rb
@@ -53,13 +53,53 @@ class App < Sinatra::Base
     User.ensure_index([[:wake_up_in_seconds_since_midnight, 1], [:go_to_sleep_in_seconds_since_midnight, 1], [:role, 1]])
     Helper.ensure_index(:lanugages)
   end
-  #logging according to: http://spin.atomicobject.com/2013/11/12/production-logging-sinatra/
-  ::Logger.class_eval { alias :write :'<<' }
-  access_log = ::File.join(::File.dirname(::File.expand_path(__FILE__)),'log','access.log')
-  access_logger = ::Logger.new(access_log, 'daily')
-  error_logger = ::File.new(::File.join(::File.dirname(::File.expand_path(__FILE__)),'log','error.log'),"a+")
-  error_logger.sync = true
 
+  def self.access_logger
+    @access_logger||= ::Logger.new(access_log, 'daily')
+    @access_logger
+  end
+
+  def self.error_logger
+    @error_logger ||= ::File.new(::File.join(::File.dirname(::File.expand_path(__FILE__)),'log','error.log'),"a+")
+    @error_logger
+  end
+
+  def self.access_log
+    @access_log ||= ::File.join(::File.dirname(::File.expand_path(__FILE__)),'log','access.log')
+    @access_log
+  end
+
+  def error_log
+    @error_log ||= ::File.new("log/error.log","a+")
+    @error_log
+  end
+
+  def self.setup_logger
+     #logging according to: http://spin.atomicobject.com/2013/11/12/production-logging-sinatra/
+    ::Logger.class_eval { alias :write :'<<' }
+    error_logger.sync = true
+    TheLogger.log.level = Logger::DEBUG  # could be DEBUG, ERROR, FATAL, INFO, UNKNOWN, WARN
+    TheLogger.log.formatter = proc { |severity, datetime, progname, msg| "[#{severity}] #{datetime.strftime('%Y-%m-%d %H:%M:%S')} : #{msg}\n" }
+  end
+
+  def self.setup_mongo
+    db_config = settings.config['database']
+    MongoMapper.connection = Mongo::Connection.new(db_config['host'])
+    MongoMapper.database = db_config['name']
+    if db_config.has_key? 'username'
+      MongoMapper.connection[db_config['name']].authenticate(db_config['username'], db_config['password'])
+    else
+     MongoMapper.connection[db_config['name']]
+    end
+  end 
+
+  def self.start_cron_jobs
+    cron_job = CronJobs.new(Helper.new, requests_helper, Rufus::Scheduler.new, WaitingRequests.new, HelperPointChecker.new)
+    cron_job.start_jobs
+  end
+ 
+  setup_logger
+ 
   # Do any configurations
   configure do
     set :environment, :development
@@ -71,32 +111,18 @@ class App < Sinatra::Base
     set :config, YAML.load_file('config/config.yml') rescue nil || {}
     set :scheduler, Rufus::Scheduler.new
     Encoding.default_external = 'UTF-8'
-
-    TheLogger.log.level = Logger::DEBUG  # could be DEBUG, ERROR, FATAL, INFO, UNKNOWN, WARN
-    TheLogger.log.formatter = proc { |severity, datetime, progname, msg| "[#{severity}] #{datetime.strftime('%Y-%m-%d %H:%M:%S')} : #{msg}\n" }
-
+   
     use ::Rack::CommonLogger, access_logger
 
     opentok_config = settings.config['opentok']
     OpenTokSDK = OpenTok::OpenTok.new opentok_config['api_key'], opentok_config['api_secret']
-
-    db_config = settings.config['database']
-    MongoMapper.connection = Mongo::Connection.new(db_config['host'])
-    MongoMapper.database = db_config['name']
-    if db_config.has_key? 'username'
-      MongoMapper.connection[db_config['name']].authenticate(db_config['username'], db_config['password'])
-    else
-     MongoMapper.connection[db_config['name']]
-    end
-    ua_config = settings.config['urbanairship']
-    @requests_helper = RequestsHelper.new ua_config, TheLogger
-    cron_job = CronJobs.new(Helper.new, @requests_helper, Rufus::Scheduler.new, WaitingRequests.new, HelperPointChecker.new)
-    cron_job.start_jobs
     
+    setup_mongo
+    start_cron_jobs
   end
-  error_log = ::File.new("log/error.log","a+")
-  setup_event_bus()
-  ensure_indeces()
+  
+  setup_event_bus
+  ensure_indeces
 
   before  do
     env["rack.errors"] = error_log

7c9f87a00323d42d709c8421cfa42f31268f1dee
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-09-02 15:39:23 +0200

    expose vagrants mongo to outside, run tests against vagrant server

--- Vagrantfile
+++ Vagrantfile
@@ -24,6 +24,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # accessing "localhost:8080" will access port 80 on the guest machine.
   config.vm.network "forwarded_port", guest: 80, host: 9090
   config.vm.network "forwarded_port", guest: 3000, host: 3001
+  config.vm.network "forwarded_port", guest: 27017, host: 27017
 
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
--- rest-specs/rest_shared_context.rb
+++ rest-specs/rest_shared_context.rb
@@ -27,8 +27,8 @@ shared_context "rest-context" do
     @username = config['authentication']['username']
     @password = config['authentication']['password']
     @security_salt = config["security_salt"]
-    @servername = "http://localhost:9292"
-    @servername_with_credentials = "http://#{@username}:#{@password}@localhost:9292"
+    @servername = "http://localhost:3001"
+    @servername_with_credentials = "http://#{@username}:#{@password}@localhost:3001"
 
     @email =  create_unique_email
     @password = encrypt_password('Password1')

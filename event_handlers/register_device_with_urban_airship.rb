909635b143faeb00b882a1868382cdcbdad974e0
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-09-24 11:36:20 +0200

    dont register device if inactive

--- event_handlers/register_device_with_urban_airship.rb
+++ event_handlers/register_device_with_urban_airship.rb
@@ -7,7 +7,7 @@ class RegisterDeviceWithUrbanAirship
     device_id = payload[:device_id]
     device = Device.first(:_id => device_id)
 
-    return if device.nil?
+    return if device.nil? || device.inactive
     @requests_helper.register_device device.development, device.device_token, :alias => device.device_name, :tags => [ device.model, device.system_version, "v" + device.app_version, "v" + device.app_bundle_version, device.locale ]
   end
 end

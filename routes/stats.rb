4004222b602154f77320707298e12b0fca80ce73
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-09-30 10:09:43 +0200

    refactor extract method

--- routes/stats.rb
+++ routes/stats.rb
@@ -13,8 +13,8 @@ class App < Sinatra::Base
       no_helped = Request.count(:helper_id => helper._id, :answered => true)
       total_points = helper.points
       events = get_point_events helper
-      current_level = helper.user_level
-      next_level = helper.user_level.next_user_level
+      current_level =  user_level_to_BMELevel helper.user_level
+      next_level = user_level_to_BMELevel helper.user_level.next_user_level 
 
       return {'no_helped' => no_helped, 'total_points' => total_points, 'events' => events, 'current_level'=> current_level, 'next_level' => next_level}.to_json
     end
@@ -24,6 +24,10 @@ class App < Sinatra::Base
   end
 
   class BMELevel < Struct.new(:title, :threshold)
+  end
+
+  def user_level_to_BMELevel user_level
+    BMELevel.new(user_level.name, user_level.point_threshold)
   end
 
   def get_point_events helper

784fae11fe9bad6efeeffd3bf94962deb9f8dca5
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-10-27 15:48:55 +0100

    find how many uses ios verson

---
+++ mongo_queries/map_reduce_ios_version.js
@@ -0,0 +1,21 @@
+db.iphone_distributions.remove({})
+
+db.devices.mapReduce(
+function(){
+  var iosVersion = this.system_version.substring(10,11);
+  emit(this.model+iosVersion, {count: 1});
+},
+function(key, values){ 
+  var count = 0;
+
+  values.forEach(function(v) {
+      count += v['count'];;
+  });
+
+  return {count:count};
+},
+{
+  query:{},
+  out:"iphone_distributions"
+  }
+  )

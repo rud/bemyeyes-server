ffb1b3831e5a0ba74e6f5b83153396ed10209c31
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-10-27 15:52:59 +0100

    beautify

--- mongo_queries/map_reduce_ios_version.js
+++ mongo_queries/map_reduce_ios_version.js
@@ -1,21 +1,24 @@
 db.iphone_distributions.remove({})
 
 db.devices.mapReduce(
-function(){
-  var iosVersion = this.system_version.substring(10,11);
-  emit(this.model+iosVersion, {count: 1});
-},
-function(key, values){ 
-  var count = 0;
+    function() {
+        var iosVersion = this.system_version.substring(10, 11);
+        emit(this.model + iosVersion, {
+            count: 1
+        });
+    },
+    function(key, values) {
+        var count = 0;
 
-  values.forEach(function(v) {
-      count += v['count'];;
-  });
+        values.forEach(function(v) {
+            count += v['count'];;
+        });
 
-  return {count:count};
-},
-{
-  query:{},
-  out:"iphone_distributions"
-  }
-  )
+        return {
+            count: count
+        };
+    }, {
+        query: {},
+        out: "iphone_distributions"
+    }
+)

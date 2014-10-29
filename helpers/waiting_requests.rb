6b31da7a440572f969a8812b4124017f01777563
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-10-29 14:33:53 +0100

    ensure correct time

--- helpers/waiting_requests.rb
+++ helpers/waiting_requests.rb
@@ -1,6 +1,6 @@
 class WaitingRequests
   #http://stackoverflow.com/questions/2943222/find-objects-between-two-dates-mongodb
   def get_waiting_requests_from_lasts_2_minutes
-    Request.where(:stopped => false, :answered => false, :created_at.gte => 2.minutes.ago).all
+    Request.where(:stopped => false, :answered => false, :created_at.gt => 2.minutes.ago.utc).all
   end
 end
